59c4be4237f534311b65f88b859106ae8527072b
Aurvyn <admin@dysrakescence.eu.org>
2025-06-29 21:56:58 -0400

    ADD: let user specify soulite file to transpile

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ mod lexer;
 mod parser;
 
 fn main() -> Result<(), String> {
-    let soulite_tree = parser::parse::<true>("test/func.sl")?;
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() == 1 || args[1] == "-h" || args[1] == "--help" {
+        return Err(format!("Usage: {} <file.sl>", args[0]));
+    }
+    let soulite_tree = parser::parse::<true>(&args[1])?;
     let rust_tree = syn::parse_file(&soulite_tree.to_rust()).unwrap();
     let rust_code = prettyplease::unparse(&rust_tree);
-    let mut file =
-        File::create("test/func.rs").map_err(|e| format!("Failed to create output file: {}", e))?;
+    let output_file = args[1].replace(".sl", ".rs");
+    let mut file = File::create(output_file).map_err(|e| format!("{}", e))?;
     file.write_all(rust_code.as_bytes())
         .map_err(|e| format!("Failed to write to output file: {}", e))?;
     Ok(())
