e9ff88e74b261b6148673d54040777db61fd2bc7
Aurvyn <admin@dysrakescence.eu.org>
2026-04-12 20:43:49 -0400

    ADD: wrap up notation

--- theories/ast.v
+++ theories/ast.v
@@ -4,12 +4,12 @@ Open Scope string_scope.
 Open Scope Z_scope.
 
 (* leave out R, Ref, Option, Result, Generic, and Array types *)
-Inductive soulite_type: Type :=
-| TypeN (bits: nat)
-| TypeZ (bits: nat)
+Inductive sl_type: Type :=
+| TypeN
+| TypeZ
 | TypeString
-| TypeList (type: soulite_type)
-| TypeClosure (param_types return_types: list soulite_type)
+| TypeList (type: sl_type)
+| TypeClosure (param_types return_type: list sl_type)
 .
 
 Inductive binop: Type :=
@@ -31,33 +31,33 @@ Inductive binop: Type :=
 .
 
 (* leave out This, None, Ref, Some, Ok, and Err expressions *)
-Inductive soulite_expr: Type :=
+Inductive sl_expr: Type :=
 | Skip
 | NExpr (n: nat) (* assume in range of bits *)
 | ZExpr (n: Z)   (* assume in range of bits *)
 | StringExpr (val: string)
 | VarExpr (name: string)
-| ListExpr (exprs: list soulite_expr)
-| BinaryExpr (op: binop) (lhs rhs: soulite_expr)
-| TernaryExpr (cond if_true if_false: soulite_expr)
-| CallExpr (name: string) (args: list soulite_expr)
+| ListExpr (exprs: list sl_expr)
+| BinaryExpr (op: binop) (lhs rhs: sl_expr)
+| TernaryExpr (cond if_true if_false: sl_expr)
+| CallExpr (name: string) (args: list sl_expr)
 (* assume that type inferrence is not used and type is always provided *)
-| DeclareExpr (name: string) (mutable: bool) (type: soulite_type) (expr: soulite_expr)
-| AssignExpr (name: string) (expr: soulite_expr)
-| ClosureExpr (args: list string) (body: soulite_expr)
-| WhileExpr (cond body: soulite_expr)
-| ReturnExpr (expr: soulite_expr)
-| SeqExpr (e1 e2: soulite_expr)
+| DeclareExpr (name: string) (mutable: bool) (type: sl_type) (expr: sl_expr)
+| AssignExpr (name: string) (expr: sl_expr)
+| ClosureExpr (args: list string) (body: sl_expr)
+| WhileExpr (cond body: sl_expr)
+| ReturnExpr (expr: sl_expr)
+| SeqExpr (e1 e2: sl_expr)
 .
 
-Record function := {
+Record sl_function := {
     name: string;
     params: list string;
-    param_types: list soulite_type;
-    body: soulite_expr;
-    return_types: list soulite_type;
+    param_types: list sl_type;
+    return_type: list sl_type;
+    body: sl_expr;
 }.
 
-Definition program := prod (list function) soulite_expr.
+Definition program := prod (list sl_function) sl_expr.
 
 Definition empty_program : program := (nil, Skip).
--- theories/notation.v
+++ theories/notation.v
@@ -1,35 +1,64 @@
-From Soulite Require Export ast.
+From Soulite Require Import ast.
 
-Coercion NExpr: nat >-> soulite_expr.
-Coercion ZExpr: Z >-> soulite_expr.
-Coercion StringExpr: string >-> soulite_expr.
+Coercion NExpr: nat >-> sl_expr.
+Coercion ZExpr: Z >-> sl_expr.
+Coercion StringExpr: string >-> sl_expr.
 
-Declare Scope soulite_scope.
-Delimit Scope soulite_scope with sl.
-Bind Scope soulite_scope with soulite_expr.
-Open Scope soulite_scope.
+Declare Custom Entry sl.
+Declare Scope sl_scope.
+Open Scope sl_scope.
 
-Infix "." := (BinaryExpr DotOp) (at level 39, left associativity): soulite_scope.
-Infix "*" := (BinaryExpr MultOp): soulite_scope.
-Infix "/" := (BinaryExpr DivOp): soulite_scope.
-Infix "+" := (BinaryExpr PlusOp): soulite_scope.
-Infix "-" := (BinaryExpr MinusOp): soulite_scope.
-Infix "<<" := (BinaryExpr ShiftLeftOp) (at level 51, left associativity): soulite_scope.
-Infix "<|" := (BinaryExpr EndLeftOp) (at level 51, left associativity): soulite_scope.
-Infix "<" := (BinaryExpr LtOp): soulite_scope.
-Infix "<=" := (BinaryExpr LteOp): soulite_scope.
-Infix ">" := (BinaryExpr GtOp): soulite_scope.
-Infix ">=" := (BinaryExpr GteOp): soulite_scope.
-Infix "==" := (BinaryExpr EqOp) (at level 70): soulite_scope.
-Infix "!=" := (BinaryExpr NotEqOp) (at level 70): soulite_scope.
-Infix "&&" := (BinaryExpr AndOp): soulite_scope.
-Infix "||" := (BinaryExpr OrOp): soulite_scope.
-Infix "=" := AssignExpr: soulite_scope.
+Notation "<{ e }>" := e
+    (e custom sl, format "'[hv' <{ '/  ' '[v' e ']' '/' }> ']'"): sl_scope.
+
+Infix "." := (BinaryExpr DotOp)
+    (in custom sl at level 10, left associativity): sl_scope.
+Infix "*" := (BinaryExpr MultOp)
+    (in custom sl at level 20, left associativity): sl_scope.
+Infix "/" := (BinaryExpr DivOp)
+    (in custom sl at level 20, left associativity): sl_scope.
+Infix "+" := (BinaryExpr PlusOp)
+    (in custom sl at level 30, left associativity): sl_scope.
+Infix "-" := (BinaryExpr MinusOp)
+    (in custom sl at level 30, left associativity): sl_scope.
+Infix "<<" := (BinaryExpr ShiftLeftOp)
+    (in custom sl at level 40, left associativity): sl_scope.
+Infix "<|" := (BinaryExpr EndLeftOp)
+    (in custom sl at level 40, left associativity): sl_scope.
+Infix "<" := (BinaryExpr LtOp)
+    (in custom sl at level 50): sl_scope.
+Infix "<=" := (BinaryExpr LteOp)
+    (in custom sl at level 50): sl_scope.
+Infix ">" := (BinaryExpr GtOp)
+    (in custom sl at level 50): sl_scope.
+Infix ">=" := (BinaryExpr GteOp)
+    (in custom sl at level 50): sl_scope.
+Infix "==" := (BinaryExpr EqOp)
+    (in custom sl at level 50): sl_scope.
+Infix "!=" := (BinaryExpr NotEqOp)
+    (in custom sl at level 50): sl_scope.
+Infix "&&" := (BinaryExpr AndOp)
+    (in custom sl at level 60, left associativity): sl_scope.
+Infix "||" := (BinaryExpr OrOp)
+    (in custom sl at level 60, left associativity): sl_scope.
 
 
+Notation "expr" := expr
+    (in custom sl at level 0, expr constr at level 0).
+Notation "f ( x .. y )" := (CallExpr f (cons x .. (cons y nil) ..))
+    (in custom sl at level 0).
+Notation "[ type ]" := (TypeList type)
+    (in custom sl at level 70).
+Notation "{ typeA .. typeZ -> ret_typeA .. ret_typeZ }" :=
+    (TypeClosure
+        (cons typeA .. (cons typeZ nil) ..)
+        (cons ret_typeA .. (cons ret_typeZ nil) ..))
+    (in custom sl at level 75).
 Notation "if_true <- cond ; if_false" := (TernaryExpr cond if_true if_false)
-    (at level 75).
+    (in custom sl at level 80, left associativity).
 Notation "name : type = expr" := (DeclareExpr name false type expr)
-    (at level 80).
+    (in custom sl at level 90, right associativity).
 Notation "name ; type = expr" := (DeclareExpr name true type expr)
-    (at level 80).
+    (in custom sl at level 90, right associativity).
+Notation "( expr )" := expr
+    (in custom sl, expr at level 99).
