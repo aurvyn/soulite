07ea28bd2395be86a1df723b86cce4776053f1dd
Aurvyn <admin@dysrakescence.eu.org>
2026-04-23 01:30:27 -0400

    ADD: `binop_eval_list`

--- theories/semantics.v
+++ theories/semantics.v
@@ -1,9 +1,28 @@
 From Stdlib Require Import String.
 From Stdlib Require Import NArith.
 From Stdlib Require Import ZArith.
+From Stdlib Require Import List.
 From stdpp Require Import gmap.
 From Soulite Require Import ast.
 From Soulite Require Import notation.
+Import ListNotations.
+
+Fixpoint list_eqb {A: Type} (eqb_A: A -> A -> bool) (l1 l2: list A): bool :=
+    match l1, l2 with
+    | [], [] => true
+    | h1 :: t1, h2 :: t2 => eqb_A h1 h2 && list_eqb eqb_A t1 t2
+    | _, _ => false
+    end.
+
+Fixpoint sl_lit_eqb (lit1 lit2: sl_lit): bool :=
+    match lit1, lit2 with
+    | LitBool b1, LitBool b2 => Bool.eqb b1 b2
+    | LitN n1, LitN n2 => N.eqb n1 n2
+    | LitZ z1, LitZ z2 => Z.eqb z1 z2
+    | LitString s1, LitString s2 => String.eqb s1 s2
+    | LitList l1, LitList l2 => list_eqb sl_lit_eqb l1 l2
+    | _, _ => false
+    end.
 
 Definition binop_eval_bool (op: binop) (b1 b2: bool): option sl_lit :=
     LitBool <$> match op with
@@ -34,23 +53,23 @@ Definition binop_eval_N (op: binop) (n1 n2: N): option sl_lit :=
     | _ => None
     end.
 
-Definition binop_eval_Z (op: binop) (n1 n2: Z): option sl_lit :=
+Definition binop_eval_Z (op: binop) (z1 z2: Z): option sl_lit :=
     match op with
-    | PlusOp => Some (LitZ (n1 + n2))
-    | MinusOp => Some (LitZ (n1 - n2))
-    | MultOp => Some (LitZ (n1 * n2))
-    | DivOp => Some (LitZ (Z.quot n1 n2))
-    | ModOp => Some (LitZ (Z.rem n1 n2))
-    | LtOp => Some (LitBool (Z.ltb n1 n2))
-    | LeOp => Some (LitBool (Z.leb n1 n2))
-    | GtOp => Some (LitBool (Z.ltb n2 n1))
-    | GeOp => Some (LitBool (Z.leb n2 n1))
-    | EqOp => Some (LitBool (Z.eqb n1 n2))
-    | NotEqOp => Some (LitBool (negb (Z.eqb n1 n2)))
-    | AndOp => Some (LitZ (Z.land n1 n2))
-    | OrOp => Some (LitZ (Z.lor n1 n2))
-    | ShiftLOp => Some (LitZ (Z.shiftl n1 n2))
-    | ShiftROp => Some (LitZ (Z.shiftr n1 n2))
+    | PlusOp => Some (LitZ (z1 + z2))
+    | MinusOp => Some (LitZ (z1 - z2))
+    | MultOp => Some (LitZ (z1 * z2))
+    | DivOp => Some (LitZ (Z.quot z1 z2))
+    | ModOp => Some (LitZ (Z.rem z1 z2))
+    | LtOp => Some (LitBool (Z.ltb z1 z2))
+    | LeOp => Some (LitBool (Z.leb z1 z2))
+    | GtOp => Some (LitBool (Z.ltb z2 z1))
+    | GeOp => Some (LitBool (Z.leb z2 z1))
+    | EqOp => Some (LitBool (Z.eqb z1 z2))
+    | NotEqOp => Some (LitBool (negb (Z.eqb z1 z2)))
+    | AndOp => Some (LitZ (Z.land z1 z2))
+    | OrOp => Some (LitZ (Z.lor z1 z2))
+    | ShiftLOp => Some (LitZ (Z.shiftl z1 z2))
+    | ShiftROp => Some (LitZ (Z.shiftr z1 z2))
     | _ => None
     end.
 
@@ -62,12 +81,21 @@ Definition binop_eval_string (op: binop) (s1 s2: string): option sl_lit :=
     | _ => None
     end.
 
+Definition binop_eval_list (op: binop) (l1 l2: list sl_lit): option sl_lit :=
+    match op with
+    | PlusOp => Some (LitList (l1 ++ l2))
+    | EqOp => Some (LitBool (list_eqb sl_lit_eqb l1 l2))
+    | NotEqOp => Some (LitBool (negb (list_eqb sl_lit_eqb l1 l2)))
+    | _ => None
+    end.
+
 Definition binop_eval (op: binop) (v1 v2: sl_val): option sl_val :=
-    match v1, v2 with
-    | LitVal (LitBool b1), LitVal (LitBool b2) => LitVal <$> binop_eval_bool op b1 b2
-    | LitVal (LitN n1), LitVal (LitN n2) => LitVal <$> binop_eval_N op n1 n2
-    | LitVal (LitZ n1), LitVal (LitZ n2) => LitVal <$> binop_eval_Z op n1 n2
-    | LitVal (LitString s1), LitVal (LitString s2) => LitVal <$> binop_eval_string op s1 s2
+    LitVal <$> match v1, v2 with
+    | LitVal (LitBool b1), LitVal (LitBool b2) => binop_eval_bool op b1 b2
+    | LitVal (LitN n1), LitVal (LitN n2) => binop_eval_N op n1 n2
+    | LitVal (LitZ n1), LitVal (LitZ n2) => binop_eval_Z op n1 n2
+    | LitVal (LitString s1), LitVal (LitString s2) => binop_eval_string op s1 s2
+    | LitVal (LitList l1), LitVal (LitList l2) => binop_eval_list op l1 l2
     | _, _ => None
     end.
 
